2177ce072301e08953db7c2ae788ec81e234a1ca
Mark Hinschberger <markhinschberger@gmail.com>
2015-11-25 11:05:39 +0800

    fixed bug not allowing post

--- client/app.js
+++ client/app.js
@@ -8,20 +8,20 @@ Template.messages.helpers({
 });
 
 
-Template.footer.events({
-  'keypress input': function(e) {
-    var inputVal = $('.input-box_text').val();
-    if(!!inputVal) {
-      var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
-      if (charCode == 13) {
-        e.stopPropagation();
-        Messages.insert({text: $('.input-box_text').val()});
-        $('.input-box_text').val("");
-        return false;
-      }    
-    }
-  }
-});
+// Template.footer.events({
+//   'keypress input': function(e) {
+//     var inputVal = $('.input-box_text').val();
+//     if(!!inputVal) {
+//       var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+//       if (charCode == 13) {
+//         e.stopPropagation();
+//         Messages.insert({text: $('.input-box_text').val()});
+//         $('.input-box_text').val("");
+//         return false;
+//       }    
+//     }
+//   }
+// });
 
 Accounts.ui.config({
     passwordSignupFields: 'USERNAME_AND_EMAIL'
--- client/input.js
+++ client/input.js
@@ -1,2 +1,19 @@
-Meteor.call('newMessage', {text: $('.input-box_text').val()});
-
+Template.footer.events({
+  'keypress input': function(e) {
+    var inputVal = $('.input-box_text').val();
+    if(!!inputVal) {
+      var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+      if (charCode == 13) {
+        e.stopPropagation();
+        Messages.insert({
+					text: inputVal,
+					user: Meteor.userId(),
+					timestamp: Date.now(),
+					channel: Session.get('channel')
+					});
+        $('.input-box_text').val("");
+        return false;
+      }    
+    }
+  }
+});
